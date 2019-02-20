d65c980cb98b572b7dfd57a1d5cb9234512eb72c
Eduard <eduard.duarte@hotmail.com>
2019-02-19 22:27:34 -0500

    add Router

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand">{{pageTitle}}</a>
+      <ul class="nav nav-pills">
+        <li class="nav-item active">
+          <a class="nav-link" [routerLink]="['/welcome']">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" [routerLink]="['/products']">Product List</a>
+        </li>
+      </ul>
+  </nav>
+  <div class='container'>
+    <router-outlet></router-outlet>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'Table de products dinamic';
+  pageTitle = 'Table de products dinamic';
 }
